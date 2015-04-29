49e92811c791af8a256b0179eacd49bb439b36fa
jcordier <jeremy.cord@gmail.com>
2015-04-29 16:05:36 +0200

    response json

--- src/lyontour/model/tour_manager.py
+++ src/lyontour/model/tour_manager.py
@@ -1,24 +1,37 @@
+from sqlalchemy import Enum
+
+
 __author__ = 'vcaen'
 
 import datetime
 import json
 
+class TypePI(Enum):
+    MUSEE = 1
+    PARC = 2
+    CAFE = 3
+
 class Tour:
 
     def __init__(self, dateDeb, dateFin):
-        self.m_DateDebut = datetime.datetime.strptime(dateDeb, '%d/%m/%Y %H:%M').date()
-        self.m_DateFin = datetime.datetime.strptime(dateFin, '%d/%m/%Y %H:%M').date()
+        self.m_DateDebut = datetime.datetime.strptime(dateDeb, '%d/%m/%Y %H:%M')
+        self.m_DateFin = datetime.datetime.strptime(dateFin, '%d/%m/%Y %H:%M')
         self.m_nbJour = (self.m_DateFin - self.m_DateDebut).days + 1
 
         self.jours = []
         for i in range(0,self.m_nbJour,1):
             self.jours.append(Jour(datetime.timedelta(i)))
 
-            response = Object()
+            response = JSONObject()
             response.DateDebut = str(self.m_DateDebut)
             response.DateFin = str(self.m_DateFin)
             response.nbJour = str(self.m_nbJour)
             response.Jours = []
+            response.PI = []
+
+            response.PI.append(PointDInteret('Beaux Arts',TypePI.MUSEE))
+            response.PI.append(PointDInteret("Tete d'Or",TypePI.PARC))
+            response.PI.append(PointDInteret("Café Mokxa",TypePI.CAFE))
 
         self.response = response
 
@@ -30,8 +43,13 @@ class Jour:
     def __init__(self, date):
         self.m_date = date
 
+class PointDInteret:
+    def __init__(self, nom, type):
+        self.m_nom = nom
+        self.m_type = type
 
-class Object:
+
+class JSONObject:
     def to_JSON(self):
         return json.dumps(self, default=lambda o: o.__dict__,
             sort_keys=True, indent=4, separators=(',', ': '))
--- src/lyontour/views/status.py
+++ src/lyontour/views/status.py
@@ -1,14 +1,14 @@
 __author__ = 'vcaen'
 from lyontour import api, app
 from flask.ext.restful import Resource
-from lyontour.model.models import Jour,Tour
+from lyontour.model.tour_manager import Tour
 from flask import Flask, request
 
 todos = {}
 print("status")
 
 
-@app.route('/', methods=['POST'])
+@app.route('/Tour', methods=['POST'])
 def Test():
     return (Tour(request.form['DateDebut'], request.form['DateFin'])).toString()
 
