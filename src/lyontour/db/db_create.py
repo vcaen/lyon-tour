d42a44d34ebfb6037c002c9e5b0ef9da9e53b00f
Mohamed Riadh Chérif <mohamed-riadh.cherif@insa-lyon.fr>
2015-04-30 19:53:38 +0200

    meteo_filter

--- src/lyontour/db/db_create.py
+++ src/lyontour/db/db_create.py
@@ -2,13 +2,29 @@
 #  -*- coding: utf-8 -*-
 
 from lyontour import db
-import os.path
 from lyontour.model.models import Section
+import os.path
 
 print "Creating Database..."
 db.drop_all()
 db.create_all()
 print "Database created"
 
+print "Filling Section table .."
+food = Section("food", "whatever", "whatever", 2)
+drinks = Section("drinks", "whatever", "evening/night", 3)
+coffee = Section("coffee", "whatever", "evening/night", 2)
+shops = Section("shops", "bad", "morning/evening", 4)
+arts = Section("arts", "bad", "evening", 2)
+outdoors = Section("outdoors", "good", "day", 3)
+sights = Section("sights", "good", "day", 3)
 
-
+db.session.add(food)
+db.session.add(drinks)
+db.session.add(coffee)
+db.session.add(shops)
+db.session.add(arts)
+db.session.add(outdoors)
+db.session.add(sights)
+db.session.commit()
+print "Section table Filled "
