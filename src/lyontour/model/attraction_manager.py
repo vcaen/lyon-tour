049e302c44bc652e05005d9803f5d1b255033047
Vadim Caen <vadimcaen@gmail.com>
2015-04-30 08:57:01 +0200

    Attraction manager not working yet but needed by jeremy

--- src/lyontour/model/attraction_manager.py
+++ src/lyontour/model/attraction_manager.py
@@ -1,1 +1,25 @@
+from lyontour.model.models import Attraction, Type
+
 __author__ = 'vcaen'
+
+
+class AttractionManager:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_attraction(self, number):
+        list_attractions = Attraction.query.limit(number).all()
+        if len(list) < number:
+            # Todo request more attractions
+            pass
+
+
+    @staticmethod
+    def get_attraction_by_type(self, number, type):
+        list_attractions = Attraction.query.filter_by(type=Type.query.filter_by(name=type))
+
+
+
+
