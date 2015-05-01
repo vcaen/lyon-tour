42ceb134abe578e27bd6e4a8d83c0f3717ea0441
Sonia Ghabi <sonia.ghabi@gmail.com>
2015-05-01 14:08:00 +0200

    ... foursquare_manager

--- src/lyontour/model/foursquare_manager.py
+++ src/lyontour/model/foursquare_manager.py
@@ -6,7 +6,6 @@ import json
 from lyontour.model import models
 from lyontour import db
 
-
 #from models import Type
 
 client_id = 'LMF4BB0X4XLOO4QGB02QU3XH20Z0HR5ZFCPHLR4KPJR4VTC0'
@@ -28,49 +27,64 @@ def executeRequests(limit, listSection):
             for val2 in val['items']:
                 id = val2['venue']['id']
                 nom = val2['venue']['name']
-                #type = val2['venue']['categories'][0]['name']
                 if 'description' not in val2['venue']:
                     description = ''
                 else:
                     description = val2['venue']['description']
-                if 'address' not in val2['venue']['location']:
-                    adresse = ''
-                else:
-                    adresse = val2['venue']['location']['address']
-                latitude = val2['venue']['location']['lat']
-                longitude = val2['venue']['location']['lng']
-                codePostal = val2['venue']['location']['postalCode']
-                ville = val2['venue']['location']['city']
+
+                if 'location' in val2['venue'] :
+
+                    if 'address' not in val2['venue']['location']:
+                        adresse = ''
+                    else:
+                        adresse = val2['venue']['location']['address']
+
+                    if 'lat' not in val2['venue']['location']:
+                        latitude = ''
+                    else:
+                        latitude = val2['venue']['location']['lat']
+
+                    if 'lng' not in val2['venue']['location']:
+                        longitude = ''
+                    else:
+                        longitude = val2['venue']['location']['lng']
+
+                    if 'postalCode' not in val2['venue']['location']:
+                        codePostal = ''
+                    else:
+                        codePostal = val2['venue']['location']['postalCode']
+
+                    if 'city' not in val2['venue']['location']:
+                        ville = ''
+                    else:
+                        ville = val2['venue']['location']['city']
+
                 if 'phone' not in val2['venue']['contact']:
                     telephone = ' '
                 else:
                     telephone = val2['venue']['contact']['phone']
 
                 attraction = models.Attraction(nom, section, description, adresse)
-                # attraction.address = adresse
-                # #attraction.description = description
-                # attraction.foursquare_id = id
+                attraction.foursquare_id = id
                 # attraction.hours = 'NULL'
-                # attraction.latitude = latitude
-                # attraction.longitude = longitude
-                # attraction.name = nom
+                attraction.latitude = latitude
+                attraction.longitude = longitude
                 # attraction.photo = 'NULL'
-                # attraction.postcode = codePostal
-                # attraction.type = type
-
+                attraction.postcode = codePostal
                 listAttraction.append(attraction)
-                
+                db.session.add(attraction)
+    for attract in listAttraction:
+        print attract.name
     return listAttraction
 
 section = models.Section()
 section.name = 'coffee'
-section.id = 1
 db.session.add(section)
 section2 = models.Section()
 section2.name = 'arts'
-section2.id = 2
 db.session.add(section2)
 listSection = []
 listSection.append(section)
 listSection.append(section2)
 executeRequests(5, listSection)
+
