cd11bf6f151ad064052906529bd4849b852f1e21
Mohamed Riadh Chérif <mohamed-riadh.cherif@insa-lyon.fr>
2015-05-05 17:00:33 +0200

    attribut weather dans Jour

--- src/lyontour/views/status.py
+++ src/lyontour/views/status.py
@@ -9,26 +9,26 @@ from flask import Flask, request
 todos = {}
 
 
-# @app.route('/tour', methods=['POST', 'GET'])
-# def Test():
-#     if(request.method == 'POST'):
-#         list = str(request.form['filtre']).split(',')
-#         return (Tour(request.form['datedebut'], request.form['datefin'], list)).toString()
-#     else:
-#         filtres = request.args.get('filtre')
-#         if filtres is None:
-#             list = {}
-#         else:
-#             list = str(request.args.get('filtre')).split(',')
-#         return (Tour(request.args.get('datedebut'),request.args.get('datefin'), list)).toString()
-
-@app.route('/')
+@app.route('/tour', methods=['POST', 'GET'])
 def Test():
-    tour = Tour('2015-06-04','2015-06-10')
-    liste_jours = tour.getJours()
-    filtre_user = ['shops', 'arts']
-    filtre_manager = filter_manager()
-    attractions = filtre_manager.getAttractionFiltred(liste_jours, filtre_user)
-    print("Hello World")
-    print ('%s',str(attractions))
-    return (str(attractions))
+    if(request.method == 'POST'):
+        list = str(request.form['filtre']).split(',')
+        return (Tour(request.form['datedebut'], request.form['datefin'], list)).toString()
+    else:
+        filtres = request.args.get('filtre')
+        if filtres is None:
+            list = {}
+        else:
+            list = str(request.args.get('filtre')).split(',')
+        return (Tour(request.args.get('datedebut'),request.args.get('datefin'), list)).toString()
+
+# @app.route('/')
+# def Test():
+#     tour = Tour('2015-06-04','2015-06-10')
+#     liste_jours = tour.getJours()
+#     filtre_user = ['shops', 'arts']
+#     filtre_manager = filter_manager()
+#     attractions = filtre_manager.getAttractionFiltred(liste_jours, filtre_user)
+#     print("Hello World")
+#     print ('%s',str(attractions))
+#     return (str(attractions))
